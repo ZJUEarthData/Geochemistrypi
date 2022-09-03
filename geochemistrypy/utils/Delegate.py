bfcdf74996a00c53b9182905009fc35411ea73e2
Jinger52035 <62539791+Jinger52035@users.noreply.github.com>
2022-09-04 00:09:39 +0800

    Add Delegate and Decorate in Python
    1.This is not about Python’s language feature called decorators , but about the design patterns called “decorator” and “delegate.”
    2.This thing will be improved
    3.This utility class doesn't affect everything at hand, and you can think of it as a macro (like C++)

---
+++ geochemistrypy/utils/Delegate.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+
+class Decorator_Base:
+    def __init__(self, model):
+        self.model = model
+        self.model_methods = [f for f in dir(type(self.model)) if not f.startswith('_')]
+        self.model_attributes = [a for a in self.model.__dict__.keys()]
+
+    def __getattr__(self, func):
+        if func in self.model_methods:
+            def method(*args):
+                return getattr(self.model, func)(*args)
+            return method
+        elif func in self.model_attributes:
+            return getattr(self.model, func)
+        else:
+            raise AttributeError
+
+class Decorator:
+    def __init__(self, model, callback_methods=[]):
+        self.model = model
+        self.model_methods = [f for f in dir(type(self.model)) if not f.startswith('_')]
+        self.model_attributes = [a for a in self.model.__dict__.keys()]
+        self.callback_methods = callback_methods
+        self.list_of_callback_methods = []
+        self.__divide_callback_functions()
+
+    def __getattr__(self, func):
+        if func in self.model_methods and func not in self.list_of_callback_methods:
+            def method(*args):
+                return getattr(self.model, func)(*args)
+            return method
+        elif func in self.model_attributes:
+            return getattr(self.model, func)
+        elif func in self.list_of_callback_methods:
+            def method(*args):
+                return self.__callback_switchboard(func, *args)
+            return method
+        else:
+            raise AttributeError
+
+    def __divide_callback_functions(self):
+        for method in self.callback_methods:
+            if 'before' in method.keys(): self.list_of_callback_methods.append(method['before'])
+            if 'after' in method.keys(): self.list_of_callback_methods.append(method['after'])
+
+    def __callback_switchboard(self, func, *args):
+        for method in self.callback_methods:
+            if 'before' in method.keys() and func == method['before']:
+                getattr(self, method['do'])()
+                getattr(self.model, func)(*args)
+            if 'after' in method.keys() and func == method['after']:
+                getattr(self.model, func)(*args)
+                getattr(self, method['do'])()
+
+class MixinDelegator(object):
+    def __getattr__(self, called_method):
+        def wrapper(*args, **kwargs):
+            delegation_config = getattr(self, 'DELEGATED_METHODS', None)
+            if not isinstance(delegation_config, dict):
+                raise AttributeError("'%s' object has not defined any delegated methods" % (self.__class__.__name__))
+            for delegate_object_str, delegated_methods in delegation_config.items():
+                if called_method in delegated_methods:
+                    break
+                else:
+                    raise AttributeError("'%s' object has no attribute '%s'" % (self.__class__.__name__, called_method))
+            delegate_object = getattr(self, delegate_object_str, None)
+            return getattr(delegate_object, called_method)(*args, **kwargs)
+        return wrapper
