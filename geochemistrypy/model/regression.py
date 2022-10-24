990485fde0ebb4e89233019b6ae2e00f1a10435a
lifang <2110205034@email.szu.edu.cn>
2022-10-24 16:03:14 +0800

    test

--- geochemistrypy/model/regression.py
+++ geochemistrypy/model/regression.py
@@ -10,6 +10,7 @@ from sklearn.preprocessing import PolynomialFeatures
 from sklearn.tree import DecisionTreeRegressor, plot_tree
 from sklearn.ensemble import ExtraTreesRegressor, RandomForestRegressor
 from sklearn.svm import SVR
+from sklearn.neural_network import MLPRegressor
 from typing import Union, Optional, List, Dict, Callable, Tuple, Any
 from typing import Sequence
 import matplotlib.pyplot as plt
@@ -18,6 +19,7 @@ import pandas as pd
 import xgboost
 from ._base import WorkflowBase
 from .func.algo_regression._polynomial import show_formula
+from .func.algo_regression._dnn import plot_pred
 # sys.path.append("..")
 
 
@@ -690,3 +692,245 @@ class SupportVectorRegression(RegressionWorkflowBase):
 
     def special_components(self):
         pass
+
+
+class DNNRegression(RegressionWorkflowBase, BaseEstimator):
+
+    name = "Deep Neural Networks"
+    special_function = ["Loss Record"]
+
+    def __init__(
+            self,
+            hidden_layer_sizes: tuple = (50, 25, 5),
+            activation: List[str] = 'relu',
+            solver: List[str] ='adam',
+            alpha: float = 0.0001,
+            batch_size: Union[int, str] ='auto',
+            learning_rate: List[str] = 'constant',
+            learning_rate_init: float = 0.001,
+            max_iter: int = 200,
+            shuffle: bool = True,
+            random_state: int = None,
+            tol: float = 1e-4,
+            verbose: bool = False,
+            warm_start: bool = False,
+            early_stopping: bool = False,
+            validation_fraction: float = 0.1,
+            beta_1: float = 0.9,
+            beta_2: float = 0.999,
+            epsilon: float = 1e-8,
+            n_iter_no_change: int = 10,
+    ):
+
+
+        """
+        Parameters
+        ----------
+        hidden_layer_sizes : tuple, length = n_layers - 2, default=(100,)
+            The ith element represents the number of neurons in the ith
+            hidden layer.
+
+        activation : {'identity', 'logistic', 'tanh', 'relu'}, default='relu'
+            Activation function for the hidden layer.
+            - 'identity', no-op activation, useful to implement linear bottleneck,
+              returns f(x) = x
+            - 'logistic', the logistic sigmoid function,
+              returns f(x) = 1 / (1 + exp(-x)).
+            - 'tanh', the hyperbolic tan function,
+              returns f(x) = tanh(x).
+            - 'relu', the rectified linear unit function,
+              returns f(x) = max(0, x)
+
+        solver : {'lbfgs', 'sgd', 'adam'}, default='adam'
+            The solver for weight optimization.
+            - 'lbfgs' is an optimizer in the family of quasi-Newton methods.
+            - 'sgd' refers to stochastic gradient descent.
+            - 'adam' refers to a stochastic gradient-based optimizer proposed by
+              Kingma, Diederik, and Jimmy Ba
+            Note: The default solver 'adam' works pretty well on relatively
+            large datasets (with thousands of training samples or more) in terms of
+            both training time and validation score.
+            For small datasets, however, 'lbfgs' can converge faster and perform
+            better.
+
+        alpha : float, default=0.0001
+            Strength of the L2 regularization term. The L2 regularization term
+            is divided by the sample size when added to the loss.
+
+        batch_size : int, default='auto'
+            Size of minibatches for stochastic optimizers.
+            If the solver is 'lbfgs', the classifier will not use minibatch.
+            When set to "auto", `batch_size=min(200, n_samples)`.
+
+        learning_rate : {'constant', 'invscaling', 'adaptive'}, default='constant'
+            Learning rate schedule for weight updates.
+            - 'constant' is a constant learning rate given by
+              'learning_rate_init'.
+            - 'invscaling' gradually decreases the learning rate ``learning_rate_``
+              at each time step 't' using an inverse scaling exponent of 'power_t'.
+              effective_learning_rate = learning_rate_init / pow(t, power_t)
+            - 'adaptive' keeps the learning rate constant to
+              'learning_rate_init' as long as training loss keeps decreasing.
+              Each time two consecutive epochs fail to decrease training loss by at
+              least tol, or fail to increase validation score by at least tol if
+              'early_stopping' is on, the current learning rate is divided by 5.
+            Only used when solver='sgd'.
+
+        learning_rate_init : float, default=0.001
+            The initial learning rate used. It controls the step-size
+            in updating the weights. Only used when solver='sgd' or 'adam'.
+
+        power_t : float, default=0.5
+            The exponent for inverse scaling learning rate.
+            It is used in updating effective learning rate when the learning_rate
+            is set to 'invscaling'. Only used when solver='sgd'.
+
+        max_iter : int, default=200
+            Maximum number of iterations. The solver iterates until convergence
+            (determined by 'tol') or this number of iterations. For stochastic
+            solvers ('sgd', 'adam'), note that this determines the number of epochs
+            (how many times each data point will be used), not the number of
+            gradient steps.
+
+        shuffle : bool, default=True
+            Whether to shuffle samples in each iteration. Only used when
+            solver='sgd' or 'adam'.
+
+        random_state : int, RandomState instance, default=None
+            Determines random number generation for weights and bias
+            initialization, train-test split if early stopping is used, and batch
+            sampling when solver='sgd' or 'adam'.
+            Pass an int for reproducible results across multiple function calls.
+            See :term:`Glossary <random_state>`.
+
+        tol : float, default=1e-4
+            Tolerance for the optimization. When the loss or score is not improving
+            by at least ``tol`` for ``n_iter_no_change`` consecutive iterations,
+            unless ``learning_rate`` is set to 'adaptive', convergence is
+            considered to be reached and training stops.
+
+        verbose : bool, default=False
+            Whether to print progress messages to stdout.
+
+        warm_start : bool, default=False
+            When set to True, reuse the solution of the previous
+            call to fit as initialization, otherwise, just erase the
+            previous solution. See :term:`the Glossary <warm_start>`.
+
+        momentum : float, default=0.9
+            Momentum for gradient descent update.  Should be between 0 and 1. Only
+            used when solver='sgd'.
+
+        nesterovs_momentum : bool, default=True
+            Whether to use Nesterov's momentum. Only used when solver='sgd' and
+            momentum > 0.
+
+        early_stopping : bool, default=False
+            Whether to use early stopping to terminate training when validation
+            score is not improving. If set to true, it will automatically set
+            aside 10% of training data as validation and terminate training when
+            validation score is not improving by at least ``tol`` for
+            ``n_iter_no_change`` consecutive epochs.
+            Only effective when solver='sgd' or 'adam'.
+
+        validation_fraction : float, default=0.1
+            The proportion of training data to set aside as validation set for
+            early stopping. Must be between 0 and 1.
+            Only used if early_stopping is True.
+
+        beta_1 : float, default=0.9
+            Exponential decay rate for estimates of first moment vector in adam,
+            should be in [0, 1). Only used when solver='adam'.
+
+        beta_2 : float, default=0.999
+            Exponential decay rate for estimates of second moment vector in adam,
+            should be in [0, 1). Only used when solver='adam'.
+
+        epsilon : float, default=1e-8
+            Value for numerical stability in adam. Only used when solver='adam'.
+
+        n_iter_no_change : int, default=10
+            Maximum number of epochs to not meet ``tol`` improvement.
+            Only effective when solver='sgd' or 'adam'.
+
+            .. versionadded:: 0.20
+
+        max_fun : int, default=15000
+            Only used when solver='lbfgs'. Maximum number of function calls.
+            The solver iterates until convergence (determined by 'tol'), number
+            of iterations reaches max_iter, or this number of function calls.
+            Note that number of function calls will be greater than or equal to
+            the number of iterations for the MLPRegressor.
+
+            .. versionadded:: 0.22
+
+        References
+        ----------------------------------------
+        Hinton, Geoffrey E. "Connectionist learning procedures."
+        Artificial intelligence 40.1 (1989): 185-234.
+        Glorot, Xavier, and Yoshua Bengio.
+        "Understanding the difficulty of training deep feedforward neural networks."
+        International Conference on Artificial Intelligence and Statistics. 2010.
+        :arxiv:`He, Kaiming, et al (2015). "Delving deep into rectifiers:
+        Surpassing human-level performance on imagenet classification." <1502.01852>`
+        :arxiv:`Kingma, Diederik, and Jimmy Ba (2014)
+        "Adam: A method for stochastic optimization." <1412.6980>`
+        """
+        super().__init__()
+        self.hidden_layer_sizes = hidden_layer_sizes
+        self.activation = activation
+        self.solver = solver
+        self.alpha = alpha
+        self.batch_size = batch_size
+        self.learning_rate = learning_rate
+        self.learning_rate_init = learning_rate_init
+        self.max_iter = max_iter
+        self.shuffle = shuffle
+        self.random_state = random_state
+        self.tol = tol
+        self.verbose = verbose
+        self.warm_start = warm_start
+        self.early_stopping = early_stopping
+        self.validation_fraction = validation_fraction
+        self.beta_1 = beta_1
+        self.beta_2 = beta_2
+        self.epsilon = epsilon
+        self.n_iter_no_change = n_iter_no_change
+
+        self.model = MLPRegressor(hidden_layer_sizes=self.hidden_layer_sizes,
+                                   activation=self.activation,
+                                   solver=self.solver,
+                                   alpha=self.alpha,
+                                   batch_size=self.batch_size,
+                                   learning_rate=self.learning_rate,
+                                   learning_rate_init=self.learning_rate_init,
+                                   max_iter=self.max_iter,
+                                   shuffle=self.shuffle,
+                                   random_state=self.random_state,
+                                   tol=self.tol,
+                                   verbose=self.verbose,
+                                   warm_start=self.warm_start,
+                                   early_stopping=self.early_stopping,
+                                   validation_fraction=self.validation_fraction,
+                                   beta_1=self.beta_1,
+                                   beta_2=self.beta_2,
+                                   epsilon=self.epsilon,
+                                   n_iter_no_change=self.n_iter_no_change)
+
+        self.naming = DNNRegression.name
+
+    def plot_learning_curve(self, algorithm_name: str, store_path):
+        print("-----* Loss Record *-----")
+        pd.DataFrame(self.model.loss_curve_).plot(title="Loss")
+        save_fig(f'Loss Record - {algorithm_name}', store_path)
+
+    @staticmethod
+    def _plot_pred(y_test_predict: pd.DataFrame, y_test: pd.DataFrame, algorithm_name: str, store_path: str):
+        print("-----* Truth v.s. Prediction *-----")
+        plot_pred(y_test_predict, y_test, algorithm_name)
+        save_fig(f'Ground Truth v.s. Prediction - {algorithm_name}', store_path)
+
+    def special_components(self, **kwargs) -> None:
+        self.plot_learning_curve(self.naming, MODEL_OUTPUT_IMAGE_PATH)
+        self._plot_pred(y_test_predict=DNNRegression.y_test_predict,
+                        y_test=DNNRegression.y_test, algorithm_name=self.naming, store_path=MODEL_OUTPUT_IMAGE_PATH)
