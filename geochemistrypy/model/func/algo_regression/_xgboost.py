3576d1ae06fbbae860070139b8fccd451a6cc2cc
Zhao Jianming <45778832+PotatoXi@users.noreply.github.com>
2022-12-03 10:36:34 +0800

    Optimize Xgboost regression (20221203)
    add 3 plots function （1.feature_importance，2.histograms_feature_weights， 3.permutation_importance）

--- geochemistrypy/model/func/algo_regression/_xgboost.py
+++ geochemistrypy/model/func/algo_regression/_xgboost.py
@@ -4,12 +4,44 @@ import pandas as pd
 import xgboost
 from matplotlib import pyplot as plt
 from sklearn.inspection import permutation_importance
-from global_variable import MODEL_OUTPUT_IMAGE_PATH
-from utils.base import save_fig
 
 
-def feature_importance(X: pd.DataFrame, X_test: pd.DataFrame, y_test: pd.DataFrame, algorithm_name: str,):
+def feature_importance(X: pd.DataFrame, algorithm_name: str):
     """Plot the Feature Importance.
+    Parameters
+    ----------
+    X: pd.DataFrame (n_samples, n_components)
+        The input data.
+
+    algorithm_name : str
+        The name of the algorithm model.
+    """
+    columns_name = X.columns
+
+    # print the feature importance value orderly
+    for feature_name, score in zip(list(columns_name),algorithm_name.feature_importances_):
+        print(feature_name, ":", score)
+    plt.figure(figsize=(40, 6))
+    plt.bar(range(len(columns_name)), algorithm_name.feature_importances_, tick_label=columns_name)
+
+
+def histograms_feature_weights(X: pd.DataFrame, algorithm_name: str):
+    """Plot the Feature Importance,  histograms present feature weights for XGBoost predictions
+    Parameters
+    ----------
+    X: pd.DataFrame (n_samples, n_components)
+        The input data.
+
+    algorithm_name : str
+        The name of the algorithm model.
+    """
+
+    columns_name = X.columns
+    plt.rcParams["figure.figsize"] = (10, 8)
+    xgboost.plot_importance(algorithm_name)
+
+def permutation_importance_(X: pd.DataFrame, X_test: pd.DataFrame, y_test: pd.DataFrame, algorithm_name: str):
+    """Plot the permutation Importance.
     Parameters
     ----------
     X: pd.DataFrame (n_samples, n_components)
@@ -25,26 +57,7 @@ def feature_importance(X: pd.DataFrame, X_test: pd.DataFrame, y_test: pd.DataFra
         The name of the algorithm model.
     """
 
-    print("-----* Feature Importance *-----")
     columns_name = X.columns
-
-    # print the feature importance value orderly
-    for feature_name, score in zip(list(columns_name),algorithm_name.feature_importances_):
-        print(feature_name, ":", score)
-
-    # histograms present feature weights for XGBoost predictions
-    plt.figure(figsize=(40, 6))
-    plt.bar(range(len(columns_name)), algorithm_name.feature_importances_, \
-            tick_label=columns_name)
-    save_fig("xgb_feature_importance", MODEL_OUTPUT_IMAGE_PATH)
-
-    # feature importance map ranked by importance
-    plt.rcParams["figure.figsize"] = (14, 8)
-    xgboost.plot_importance(algorithm_name)
-    save_fig("xgb_feature_importance_score", MODEL_OUTPUT_IMAGE_PATH)
-
-    # Permutation importance
-    print("-----* Permutation Importance  *-----")
     plt.figure(figsize=(10, 8))
     result = permutation_importance(algorithm_name, X_test,
                                     y_test, n_repeats=10,
@@ -55,5 +68,3 @@ def feature_importance(X: pd.DataFrame, X_test: pd.DataFrame, y_test: pd.DataFra
         vert=False,
         labels=np.array(columns_name),
     )
-    save_fig("xgb_feature_importance_T", MODEL_OUTPUT_IMAGE_PATH)
-
