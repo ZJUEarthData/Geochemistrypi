e44c5dafa013467f601552c95a8d986c7f7cccd9
sanyhe <sanyhew1097618435@163.com>
2023-05-20 09:25:17 +0800

    feat: append prettier and eslint for code formatting and checking to frontend code.

--- geochemistrypi/frontend/src/App.tsx
+++ geochemistrypi/frontend/src/App.tsx
@@ -1,15 +1,15 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App = () => {
     return (
         <div className="App">
-        <h1>Geochemistry π</h1>
-        <Router>
-            <Routes>
-                <Route path="/" element={<h1>Home</h1>} />
-            </Routes>
-        </Router>
+            <h1>Geochemistry π</h1>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<h1>Home</h1>} />
+                </Routes>
+            </Router>
         </div>
     );
 };
