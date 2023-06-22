21afa6eb1c1e7051863199f8068f0f93ef22f876
sanyhe <sanyhew1097618435@163.com>
2023-06-22 11:51:52 +0800

    feat: use .env to manage environment variables.

--- geochemistrypi/frontend/src/components/Dashboard.tsx
+++ geochemistrypi/frontend/src/components/Dashboard.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const Dashbord = () => {
-    return <iframe src="http://localhost:8000/dash" title="Dashboard" width="100%" height="100%" frameBorder="0"></iframe>;
+    const dashboardUrl = process.env.REACT_APP_DASHBOARD_URL || 'http://localhost:8000/dash';
+
+    return <iframe src={dashboardUrl} title="Dashboard" width="100%" height="100%" frameBorder="0"></iframe>;
 };
 
 export default Dashbord;
