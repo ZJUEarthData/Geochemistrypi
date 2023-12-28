d1acbcc8e237b8add97da4271e42fa5eb862c129
jin <571979568@qq.com>
2023-12-28 19:06:12 +0800

    docs: Adjust the style of the document

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,9 +31,9 @@ language = "EN"
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 
-# html_theme = "furo"
+html_theme = "furo"
 # html_theme = 'classic'
-html_theme = "sphinx_rtd_theme"
+# html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_static_path = ["_static"]
 
