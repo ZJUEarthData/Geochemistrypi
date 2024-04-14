07b5d5ec6d7d5764dee461341b03ba706032b937
trenki <urnottrenki@foxmail.com>
2024-04-14 21:37:57 +0800

    feat: delete old frontend project

--- geochemistrypi/frontend/src/components/Dashboard.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Dashbord = () => {
-    const dashboardUrl = process.env.REACT_APP_DASHBOARD_URL || 'http://localhost:8000/dash';
-
-    return <iframe src={dashboardUrl} title="Dashboard" width="100%" height="100%" frameBorder="0"></iframe>;
-};
-
-export default Dashbord;
