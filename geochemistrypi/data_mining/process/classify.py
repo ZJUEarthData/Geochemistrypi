108315e4b9f1642036f3c456ba699c308da1054b
Haibin Lai <12211612@mail.sustech.edu.cn>
2024-11-17 21:22:04 +0800

    feat: Adding AdaBoost Classification

--- geochemistrypi/data_mining/process/classify.py
+++ geochemistrypi/data_mining/process/classify.py
@@ -15,6 +15,7 @@ from ..model.classification import (
     MLPClassification,
     RandomForestClassification,
     SGDClassification,
+    AdaBoostClassification,
     SVMClassification,
     XGBoostClassification,
 )
@@ -139,6 +140,14 @@ class ClassificationModelSelection(ModelSelectionBase):
                 subsample=hyper_parameters["subsample"],
                 loss=hyper_parameters["loss"],
             )
+        elif self.model_name == "AdaBoost":
+            hyper_parameters = AdaBoostClassification.manual_hyper_parameters()
+            self.clf_workflow = AdaBoostClassification(
+                n_estimators=hyper_parameters["n_estimators"],
+                learning_rate=hyper_parameters["learning_rate"],
+                max_depth=hyper_parameters['max_depth'],
+                # min_samples_split=hyper_parameters['min_samples_split']
+            )
         elif self.model_name == "K-Nearest Neighbors":
             hyper_parameters = KNNClassification.manual_hyper_parameters()
             self.clf_workflow = KNNClassification(
@@ -240,6 +249,8 @@ class ClassificationModelSelection(ModelSelectionBase):
             self.clf_workflow = ExtraTreesClassification()
         elif self.model_name == "Gradient Boosting":
             self.clf_workflow = GradientBoostingClassification()
+        elif self.model_name == "AdaBoost":
+            self.clf_workflow = AdaBoostClassification()
         elif self.model_name == "K-Nearest Neighbors":
             self.clf_workflow = KNNClassification()
         elif self.model_name == "Stochastic Gradient Descent":
